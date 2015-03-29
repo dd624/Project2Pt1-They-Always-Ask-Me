bd060b6b85d90f74ff5a95ce81522b91d33c106f
dd624 <weiylcal@v1003th209.sju.edu>
2015-03-29 17:41:43 -0400

    initial

---
+++ js/faq.js
@@ -0,0 +1,233 @@
+                  
+function toggle() {
+	var question = document.getElementById("a1");
+	var text = document.getElementById("q1");
+	if(question.style.display == "block") {
+    		question.style.display = "none";
+  	}
+	else {
+		question.style.display = "block";
+	}
+} 
+
+                  
+function toggle2() {
+	var question = document.getElementById("a2");
+	var text = document.getElementById("q2");
+	if(question.style.display == "block") {
+    		question.style.display = "none";
+  	}
+	else {
+		question.style.display = "block";
+	}
+} 
+
+                  
+function toggle3() {
+	var question = document.getElementById("a3");
+	var text = document.getElementById("q3");
+	if(question.style.display == "block") {
+    		question.style.display = "none";
+  	}
+	else {
+		question.style.display = "block";
+	}
+} 
+
+function toggle4() {
+	var question = document.getElementById("a4");
+	var text = document.getElementById("q4");
+	if(question.style.display == "block") {
+    		question.style.display = "none";
+  	}
+	else {
+		question.style.display = "block";
+	}
+} 
+
+function toggle5() {
+	var question = document.getElementById("a5");
+	var text = document.getElementById("q5");
+	if(question.style.display == "block") {
+    		question.style.display = "none";
+  	}
+	else {
+		question.style.display = "block";
+	}
+} 
+
+function toggle6() {
+	var question = document.getElementById("a6");
+	var text = document.getElementById("q6");
+	if(question.style.display == "block") {
+    		question.style.display = "none";
+  	}
+	else {
+		question.style.display = "block";
+	}
+} 
+
+function toggle7() {
+	var question = document.getElementById("a7");
+	var text = document.getElementById("q7");
+	if(question.style.display == "block") {
+    		question.style.display = "none";
+  	}
+	else {
+		question.style.display = "block";
+	}
+} 
+
+function toggle8() {
+	var question = document.getElementById("a8");
+	var text = document.getElementById("q8");
+	if(question.style.display == "block") {
+    		question.style.display = "none";
+  	}
+	else {
+		question.style.display = "block";
+	}
+} 
+
+function toggle9() {
+	var question = document.getElementById("a9");
+	var text = document.getElementById("q9");
+	if(question.style.display == "block") {
+    		question.style.display = "none";
+  	}
+	else {
+		question.style.display = "block";
+	}
+} 
+
+function toggle10() {
+	var question = document.getElementById("a10");
+	var text = document.getElementById("q10");
+	if(question.style.display == "block") {
+    		question.style.display = "none";
+  	}
+	else {
+		question.style.display = "block";
+	}
+} 
+
+function toggle11() {
+	var question = document.getElementById("a11");
+	var text = document.getElementById("q11");
+	if(question.style.display == "block") {
+    		question.style.display = "none";
+  	}
+	else {
+		question.style.display = "block";
+	}
+} 
+
+function toggle12() {
+	var question = document.getElementById("a12");
+	var text = document.getElementById("q12");
+	if(question.style.display == "block") {
+    		question.style.display = "none";
+  	}
+	else {
+		question.style.display = "block";
+	}
+} 
+
+function toggle13() {
+	var question = document.getElementById("a13");
+	var text = document.getElementById("q13");
+	if(question.style.display == "block") {
+    		question.style.display = "none";
+  	}
+	else {
+		question.style.display = "block";
+	}
+} 
+
+function toggle14() {
+	var question = document.getElementById("a14");
+	var text = document.getElementById("q14");
+	if(question.style.display == "block") {
+    		question.style.display = "none";
+  	}
+	else {
+		question.style.display = "block";
+	}
+} 
+
+function toggle15() {
+	var question = document.getElementById("a15");
+	var text = document.getElementById("q15");
+	if(question.style.display == "block") {
+    		question.style.display = "none";
+  	}
+	else {
+		question.style.display = "block";
+	}
+} 
+
+function toggle16() {
+	var question = document.getElementById("a16");
+	var text = document.getElementById("q16");
+	if(question.style.display == "block") {
+    		question.style.display = "none";
+  	}
+	else {
+		question.style.display = "block";
+	}
+} 
+
+function toggle17() {
+	var question = document.getElementById("a17");
+	var text = document.getElementById("q17");
+	if(question.style.display == "block") {
+    		question.style.display = "none";
+  	}
+	else {
+		question.style.display = "block";
+	}
+} 
+
+function toggle18() {
+	var question = document.getElementById("a18");
+	var text = document.getElementById("q18");
+	if(question.style.display == "block") {
+    		question.style.display = "none";
+  	}
+	else {
+		question.style.display = "block";
+	}
+} 
+
+function toggle19() {
+	var question = document.getElementById("a19");
+	var text = document.getElementById("q19");
+	if(question.style.display == "block") {
+    		question.style.display = "none";
+  	}
+	else {
+		question.style.display = "block";
+	}
+} 
+
+function toggle20() {
+	var question = document.getElementById("a20");
+	var text = document.getElementById("q20");
+	if(question.style.display == "block") {
+    		question.style.display = "none";
+  	}
+	else {
+		question.style.display = "block";
+	}
+} 
+
+function toggle21() {
+	var question = document.getElementById("a21");
+	var text = document.getElementById("q21");
+	if(question.style.display == "block") {
+    		question.style.display = "none";
+  	}
+	else {
+		question.style.display = "block";
+	}
+} 
